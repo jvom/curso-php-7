67941b7df545e1aa66bc5a4be06343566686316a
Victor Miranda <viictoroliver@gmail.com>
2018-08-16 00:08:30 -0300

    Aula de Interface

---
+++ src/interface.php
@@ -0,0 +1,35 @@
+<?php
+/* Interface: Define padrões */
+
+interface Veiculo {
+
+    public function acelerar($velocidade);
+    public function frenar($velocidade);
+    public function trocarMarcha($marcha);
+}
+
+ class Civi implements Veiculo {
+
+
+
+    public function acelerar($velocidade) {
+        echo "O veículo acelerou até " .$velocidade. " km/h";
+    }
+    public function frenar($velocidade) {
+        echo "O veículo frenou até " .$velocidade. " km/h";
+    }
+    public function trocarMarcha($marcha) {
+        echo "O veículo engatou a marcha " .$marcha;
+
+    }
+ }
+
+ $carro = new Civic();
+
+ $carro->trocarMarcha(1);
+
+
+
+
+
+?>
